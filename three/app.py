bd9335d656a93c8bb4d19d8090d70262f68d39ef
Rev4n28 <pavlik.kolev@mail.ru>
2025-07-22 21:28:08 +0500

    change main

--- three/app.py
+++ three/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,9 +16,77 @@ class Todo(db.Model):
         return f"<Task {self.id}>"
 
 
-@app.route("/")
+# @app.route("/", methods=['POST', 'GET'])
+# def index():
+#     if request.method == "POST":
+#         task_content = request.form['context']
+#         new_task = Todo(content=task_content)
+#
+#         try:
+#             db.session.add(new_task)
+#             db.session.commit()
+#             return redirect("/")
+#         except Exception as e:
+#             return f"Не удалось добавить вашу задачу {e}"
+#     else:
+#         tasks = Todo.query.order_by(Todo.date_created).all()
+#         return render_template("index.html", tasks=tasks)
+
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        task_content = request.form['context']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"Не удалось добавить вашу задачу {e}"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        return f"Не удалось удалить задачу {e}"
+
+
+# @app.route('/update/<int:id>', methods=['POST', 'GET'])
+# def update(id):
+#     task = Todo.query.get_or_404(id)
+#     if request.method == "POST":
+#         task.content = request.form['context']
+#         try:
+#             db.session.commit()
+#             return redirect("/")
+#         except Exception as e:
+#             return f"Не удалось обновить задачу {e}"
+#     else:
+#         return render_template('update.html', task=task)
+
+@app.route('/update/<int:id>', methods=["POST", "GET"])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form['context']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            return f"Не удалось обновить задачу {e}"
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == "__main__":
